6eac6b6c4fd8f6ca02bda007189e027c54923c6f
fassane <fassanebolly@gmail.com>
2022-04-14 17:38:50 +0000

    create choose orientation sexuel component

---
+++ src/components/AppChooseOrientationSex.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { 
+    View, 
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Dimensions } from 'react-native'
+
+
+const AppChooseOrientationSex = ({inputTitle}) => {
+
+    const OneChoiceComponent = ({choice}) => {
+        return (
+        <TouchableOpacity style={styles.choiceContainer}>
+            <Text style={styles.choiceText}> {choice} </Text>
+        </TouchableOpacity>
+        )
+    }
+
+
+  return (
+    <>
+        <View style={styles.inputTitleContainer}>
+        <Text style={styles.inputTitle}> {inputTitle} </Text>
+        </View>    
+        
+        <OneChoiceComponent choice='Heterosexuelle' />
+        <OneChoiceComponent choice='Bisexuelle' />
+        <OneChoiceComponent choice='Monosexuelle' />
+   
+    </>
+  )
+}
+
+export default AppChooseOrientationSex
+
+const {width, height} = Dimensions.get('screen')
+
+const styles = StyleSheet.create({
+    choiceContainer: {
+        width: width * 0.55,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        padding: 10,
+        borderColor: '#eb5a6d',
+        borderRadius: 30,
+        backgroundColor: '#eb5a6d',
+        marginTop: 5
+    },
+    choiceText: {
+        fontSize: 16,
+        fontFamily: 'Gotham Rounded Bold',
+        color: 'white'
+    },
+    inputTitleContainer: {
+        left: -width * 0.15,
+        width: width * 0.93,
+        height: 30,
+        justifyContent: 'center',
+        position: 'relative',
+        left: 0,
+        marginTop: 10
+    },
+    inputTitle: {
+        fontSize: 16,
+        fontFamily: 'Gotham Rounded Bold',
+        
+    },
+})

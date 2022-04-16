9daf4caa2b5020a42aecd063fb4b6465ccfe46ab
fassane <fassanebolly@gmail.com>
2022-04-16 00:43:34 +0000

    new file for schools or university list

---
+++ src/utilities/SchoolOrUniversityList.js
@@ -0,0 +1,42 @@
+export const Schools = [
+    {
+        id: 1,
+        name: 'ESMT'
+    },
+    {
+        id: 2,
+        name: 'UCAD'
+    },
+    {
+        id: 3,
+        name: 'EUROMED'
+    },
+    {
+        id: 4,
+        name: 'EFI'
+    },
+    {
+        id: 5,
+        name: 'ENSAE'
+    },
+    {
+        id: 6,
+        name: 'ENSP'
+    },
+    {
+        id: 7,
+        name: 'ESI'
+    },
+    {
+        id: 8,
+        name: 'UGB'
+    },
+    {
+        id: 9,
+        name: 'ESMVI'
+    },
+    {
+        id: 10,
+        name: 'NAZA'
+    },
+]

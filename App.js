95b8ed5e9cdb3e9e865add7dc0c634ebaf1288cf
fassane <fassanebolly@gmail.com>
2022-05-24 01:15:08 +0000

    add logbox to ignore PropsTypes logs

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, {useEffect} from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import AuthNav from './src/navigation/AuthNav';
-import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen';
+import {LogBox} from "react-native"
 
 
 
 export default function App() {
 
+
+  LogBox.ignoreLogs([
+    "exported from 'deprecated-react-native-prop-types'.",
+  ])
 
   //Hide Splash screen on app load.
   useEffect(() => {
